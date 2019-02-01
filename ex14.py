d8e36ef63a8a8b59a83826c8ec01f3bc7160a3a0
mano1199 <46454509+mano1199@users.noreply.github.com>
2019-02-01 19:36:41 +0530

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -1,5 +1,8 @@
-a=int(input())
-b=int(input())
-for c in range(a+1,b+1):
-	if(c%2==1):
-		print(c)
+a=input()
+b=a.split()
+c=int(b[0])
+d=int(b[1])
+if c and d <=100000:
+	for e in range(c+1,d):
+		if e%2 ==1:
+			print(e)		
