babd2f9d2e4db061a6f44383a2df7f70ce8a7177
mano1199 <46454509+mano1199@users.noreply.github.com>
2019-07-03 18:15:54 +0530

    Create pro3.py

---
+++ pro3.py
@@ -0,0 +1,9 @@
+#mano
+sam,se=input().split()
+ram=abs(len(sam)-len(se))
+for i in range(len(sam)):
+    if len(se)==1 and se[i] in sam:
+        break
+    if sam[i]!=se[i]:
+        ram+=1
+print(ram)
